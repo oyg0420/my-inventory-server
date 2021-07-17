487af8bdb1cf40d2bc4d67709ead06c5907bedf2
oyg0420 <oyg900420@gmail.com>
2021-07-17 23:57:09 +0900

    feat(): node expresss typescript 환경셋팅
    https://spendit.atlassian.net/browse/<issue>

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '../', 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
