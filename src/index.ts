b2a36da5cbe40a3c73d765f567e88322e15e0562
oyg0420 <oyg900420@gmail.com>
2021-07-27 11:20:31 +0900

    feat(db): MongoDB connect 함수 작성

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express, { Express } from 'express';
+import connectDB from './loaders/db';
+import * as dotenv from 'dotenv';
 
 const app: Express = express();
+dotenv.config();
+connectDB();
 
 app.use(express.urlencoded());
 app.use(express.json());
---
+++ src/loaders/db.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log(`MongoDB Connected... 🚀 `);
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
